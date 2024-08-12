e0e291c837986595f2c0341e726aa3aaf2d83352
TalhaTahir24 <165506239+TalhaTahir24@users.noreply.github.com>
2024-08-12 13:53:54 +0500

    Create modules.py

---
+++ chapter1/modules.py
@@ -0,0 +1,14 @@
+# Developer = Talha Tahir
+import pyjokes
+
+# print("Printing Jokes...")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+
+# so thanks
+# that was my program
+# another line
+# Yet another line
